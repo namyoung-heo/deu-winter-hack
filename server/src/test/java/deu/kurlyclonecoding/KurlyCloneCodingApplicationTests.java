b1be78dabb5ebf52fb0ed5cf84a9dc7057c56476
Jinhui <jda0526@naver.com>
2023-01-02 18:31:17 +0900

    SpringBoot init

---
+++ server/src/test/java/deu/kurlyclonecoding/KurlyCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package deu.kurlyclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlyCloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
